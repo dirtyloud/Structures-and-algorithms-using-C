b02cc9c73e75a46ae85aa0066289b20501cb6846
dirtyloud <118288714+dirtyloud@users.noreply.github.com>
2023-04-12 21:11:32 +0300

    Create main(doubly).c

---
+++ Ring implentation/main(doubly).c
@@ -0,0 +1,17 @@
+#include "functions.h"
+
+int main(){
+    struct Ring* head = NULL;
+    pushAfter(&head, 1);
+    pushAfter(&head, 2);
+    pushAfter(&head, 3);
+    popBefore(&head);
+    pushAfter(&head, 5);
+    pushAfter(&head, 10);
+    pushBefore(&head, 15);
+    pushBefore(&head, 11);
+    popBefore(&head);
+    popAfter(&head);
+    printRing(head);
+    return 0;
+}
