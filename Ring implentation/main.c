db8403924497d98aef32e4b5ae58c3bdcfc7846c
dirtyloud <118288714+dirtyloud@users.noreply.github.com>
2023-04-12 20:02:14 +0300

    Update main.c

--- Ring implentation/main.c
+++ Ring implentation/main.c
@@ -3,15 +3,11 @@
 int main() {
     struct Ring* head = NULL;
     head = pushAfter(head, 1);
-    head = pushAfter(head, 2);
-    head = pushAfter(head, 3);
-    head = pushAfter(head, 4);
-    head = pushBefore(head, 5);
-    head = popAfter(head);
-    head = popBefore(head);
-    head = popBefore(head);
-    head = popAfter(head);
-    head = popAfter(head);
+    pushAfter(head, 2);
+    pushAfter(head,4);
+    pushBefore(head, 3);
+    popBefore(head);
+    popAfter(head);
     outputRing(head);
     return 0;
 }
